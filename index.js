a3078b8c31d3627655552404d42e97379b9cee6d
Joseph Adams <josephdadams@gmail.com>
2023-10-09 10:09:03 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // GlobalCache iTach IP2CC
 
-const { InstanceBase, InstanceStatus, Regex, runEntrypoint, TCPHelper } = require('@companion-module/base')
+const { InstanceBase, InstanceStatus, runEntrypoint } = require('@companion-module/base')
 const UpgradeScripts = require('./src/upgrades')
 
 const config = require('./src/config')
@@ -26,6 +26,7 @@ class itac_cc extends InstanceBase {
 		})
 
 		this.socket = undefined
+		this.pollTimer = undefined
 
 		this.CHOICES_PORTS = [
 			{ id: '1', label: 'Port 1' },
@@ -81,95 +82,6 @@ class itac_cc extends InstanceBase {
 		this.initPresets()
 
 		this.initTCP()
-	}
-
-	initTCP() {
-		let self = this;
-
-		if (self.socket !== undefined) {
-			self.socket.destroy();
-			delete self.socket;
-		}
-	
-		if (self.config.host) {
-			self.socket = new TCPHelper(this.config.host, 4998);
-
-			self.socket.on('connect', () => {
-				self.updateStatus(InstanceStatus.Ok);
-				self.log('debug', 'Connected');
-				self.startPolling();
-			});
-
-			self.socket.on('data', (data) => {
-				self.log('debug', 'Received: ' + data);
-				let lines = data.toString().split('\r\n');
-				for (let i = 0; i < lines.length; i++) {
-					let line = lines[i];
-					let matches = line.match(/state,(\d+):(\d+),(\d+)/);
-					if (matches) {
-						let port = matches[2];
-						let state = matches[3];
-						self.updatePortState(port, state);
-					}
-				}
-			});
-	
-			self.socket.on('error', function (err) {
-				self.log('error',"Network error: " + err.message);
-				clearInterval(self.pollTimer);
-			});
-		}
-	}
-
-	startPolling() {
-		let self = this;
-		if (self.config.poll_interval > 0) {
-			self.pollTimer = setInterval(() => {
-				self.getStates();
-			}, self.config.poll_interval);
-		}
-	}
-
-	getStates() {
-		let self = this;
-		for (let i = 1; i <= self.config.ports; i++) {
-			let cmd = `getstate,1:${i}`;
-			self.sendCommand(cmd);
-		}
-	}
-
-	updatePortState(port, state) {
-		let self = this;
-
-		for (let i = 0; i < self.DATA.length; i++) {
-			let portObj = self.DATA[i];
-			if (self.DATA[i].port == port) {
-				self.DATA[i].state = state;
-				self.checkFeedbacks('relaystate');
-				self.checkVariables();
-				break;
-			}
-		}
-	}
-
-	sendCommand(cmd) {
-		let self = this;
-
-		if (cmd !== undefined) {
-			if (self.socket !== undefined && self.socket.isConnected) {
-				self.log('debug', 'Sending: ' + cmd);
-				self.socket.send(cmd + '\r\n')
-				.then((result) => {
-					//console.log('send result: ' + result);
-				})
-				.catch((error) => {
-					//console.log('send error: ' + error);
-				});
-			} else {
-				self.log('error', 'Network error: Connection to Device not opened.')
-				clearInterval(self.pollTimer);
-			}
-		}
 	}
 }
 
